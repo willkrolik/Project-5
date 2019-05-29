2920485a6da49c3a079cd24345d6954470244ab3
willkrolik <willkrolik@gmail.com>
2019-05-29 13:04:04 -0700

    5.29 changes

--- API Project/scripts.js
+++ API Project/scripts.js
@@ -1,62 +1,81 @@
-
-window.onload = function(){
+window.onload = function() {
   // your JS here
 
-const gallery = $(".gallery");
+  const gallery = $(".gallery");
 
-
-
-var request = new XMLHttpRequest();
-request.open("GET", "https://randomuser.me/api/?results=12", true);
-request.onload = function() {
-  // Begin accessing JSON data here
-  var data = JSON.parse(this.response);
-  if (request.status >= 200 && request.status < 400) {
-    data.results.forEach(person => {
-      const employeeHtml = `<div class="card">
+  var request = new XMLHttpRequest();
+  request.open("GET", "https://randomuser.me/api/?results=12", true);
+  request.onload = function() {
+    // Begin accessing JSON data here
+    var data = JSON.parse(this.response);
+    if (request.status >= 200 && request.status < 400) {
+      data.results.forEach(person => {
+        const employeeHtml = `<div class="card">
       <div class="card-img-container">
-          <img class="card-img" src="${person.picture.medium}" alt="profile picture">
+          <img class="card-img" src="${
+            person.picture.medium
+          }" alt="profile picture">
       </div>
       <div class="card-info-container">
-          <h3 id="name" class="card-name cap">${person.name.first} ${person.name.last}</h3>
+          <h3 id="name" class="card-name cap">${person.name.first} ${
+          person.name.last
+        }</h3>
           <p class="card-text">${person.email}</p>
-          <p class="card-text cap">city, ${person.location.state}</p>
+          <p class="card-text cap">${person.location.city}, ${
+          person.location.state
+        }</p>
       </div>
       </div>`;
-      
-      
-      $('.card').click(function(){
-        $(this).find('.extra').trigger('click');
-      });
-     
 
-      const p3 = document.createElement("p");
-      p3.textContent = `${person.location.state}`;
+        const modalHtml = `<div class="modal-container">
+      <div class="modal">
+          <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+          <div class="modal-info-container">
+              <img class="modal-img" src="${
+                person.picture.medium
+              }" alt="profile picture">
+              <h3 id="name" class="modal-name cap">${person.name.first} ${
+          person.name.last
+        }</h3>
+              <p class="modal-text">${person.email}</p>
+              <p class="modal-text cap">${person.location.city}</p>
+              <hr>
+              <p class="modal-text">Phone Number: ${person.phone}</p>
+              <p class="modal-text">${person.location.street}, ${
+          person.location.city
+        }, ${person.location.state}, ${person.location.postcode}</p>
+              <p class="modal-text">Birthday: ${person.dob.date.slice(
+                0,
+                10
+              )}</p>
+          </div>
+      </div>
 
-      const a = document.createElement("a");
-      a.innerHTML = `<a href="#openModal" class="extra">More Info</a>`;
-      gallery.append(employeeHtml);
       
-      
-      
+  </div>`;
+        const card = $(".card");
 
-      $(".extra")
-        .unbind()
-        .on("click", e => {
-          const pTag = $(".lineOne");
-          pTag[0].innerHTML = e.originalEvent.path[2].id;
-          console.log(e.originalEvent.path[2].id);
+        card.unbind().click(function() {
+          const b = document.createElement("a");
+          b.innerHTML = `<a href="#openModal">More Info</a>`;
+          card.append(modalHtml);
         });
 
-  
-    });
-  } else {
-    const errorMessage = document.createElement("marquee");
-    errorMessage.textContent = `Gah, it's not working!`;
-    app.appendChild(errorMessage);
-  }
+        const p3 = document.createElement("p");
+        p3.textContent = `${person.location.state}`;
+
+        const a = document.createElement("a");
+        a.innerHTML = `<a href="#openModal">More Info</a>`;
+        gallery.append(employeeHtml);
+
+        
+      });
+    } else {
+      const errorMessage = document.createElement("marquee");
+      errorMessage.textContent = `Gah, it's not working!`;
+      app.appendChild(errorMessage);
+    }
+  };
+
+  request.send();
 };
-
-request.send();}
-
-  
